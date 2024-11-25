6779813faa01b400c2ed13a02f3e10b66d77b5b5
Alessandro <alespbecker@gmail.com>
2024-11-25 09:49:10 -0300

    Primeiro commit - Configuração inicial do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html', // Ajuste para o seu ambiente
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
